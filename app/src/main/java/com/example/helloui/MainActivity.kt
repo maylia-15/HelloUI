e88189d54bf847ec610ce7f40e2fb736b30ed3f9
maylia-15 <rabiahriska8@gmail.com>
2026-02-11 17:22:21 +0800

    fix

--- app/src/main/java/com/example/helloui/MainActivity.kt
+++ app/src/main/java/com/example/helloui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.helloui // Sesuaikan package name kamu
+package com.example.helloui
 
 import android.os.Bundle
 import android.widget.Button
@@ -11,39 +11,34 @@ import com.google.android.material.textfield.TextInputEditText
 class MainActivity : AppCompatActivity() {
 
     private lateinit var tvHasil: TextView
-    private lateinit var etNama: TextInputEditText // Tipe berubah jadi TextInputEditText
+    private lateinit var etNama: TextInputEditText
     private lateinit var switchMode: SwitchMaterial
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Inisialisasi View
         etNama = findViewById(R.id.etNama)
         tvHasil = findViewById(R.id.tvHasil)
         switchMode = findViewById(R.id.switchMode)
         val btnSapa = findViewById<Button>(R.id.btnSapa)
 
-        // Restore State (Agar tulisan tidak hilang saat ganti tema)
         if (savedInstanceState != null) {
             tvHasil.text = savedInstanceState.getString("STATE_HASIL")
         }
 
-        // Logic Tombol Sapa
         btnSapa.setOnClickListener {
             val nama = etNama.text.toString().trim()
             if (nama.isNotEmpty()) {
-                tvHasil.text = "Hello, $nama! 👋" // Ada emoji tangan
+                tvHasil.text = "Hello, $nama! 👋"
             } else {
                 etNama.error = "Nama wajib diisi!"
             }
         }
 
-        // Logic Switch Mode (KEMBALI KE TEKS)
         val currentMode = AppCompatDelegate.getDefaultNightMode()
         val isDarkMode = (currentMode == AppCompatDelegate.MODE_NIGHT_YES)
 
-        // Set kondisi awal
         switchMode.isChecked = isDarkMode
         updateSwitchText(isDarkMode)
 
@@ -53,11 +48,9 @@ class MainActivity : AppCompatActivity() {
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
-            // Teks akan update otomatis karena activity di-recreate
         }
     }
 
-    // Fungsi helper untuk ubah teks switch
     private fun updateSwitchText(isDarkMode: Boolean) {
         if (isDarkMode) {
             switchMode.text = "Mode Gelap 🌙"
